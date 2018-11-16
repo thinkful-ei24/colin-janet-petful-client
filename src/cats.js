5e7aea080690149a2c2313371b57064c3660fd69
Janet Leon <leon.irenejanet@gmail.com>
2018-11-16 08:41:46 -0800

    adds images to dog and cats components

--- src/cats.js
+++ src/cats.js
@@ -5,6 +5,7 @@ export default class Dogs extends React.Component{
 render(){
 return(
   <section className='adoptCat'>
+    <img src='https://images.unsplash.com/photo-1526509429168-2e43f01f6b81?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjM5OTI2fQ&s=4089f8e80dcf641fa46315683f1aae1e'></img>
   <header>
     <h2>Cats</h2>
   <h3>Name: Place Holder</h3>
--- src/dogs.js
+++ src/dogs.js
@@ -9,6 +9,7 @@ return(
   <section className='adoptDog'>
   <header>
     <h2>Dogs</h2>
+    <img src='https://images.unsplash.com/photo-1534628526458-a8de087b1123?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjM5OTI2fQ&s=ad020057ed4d4d17562142093cd9af3f'></img>
   <h3>Name: Place Holder</h3>
   <dl>
   <dd>sex: Place Holder</dd>
