a30b72d476047ae0a494b3ee424f6181b3b93c6d
Janet Leon <leon.irenejanet@gmail.com>
2018-11-16 10:26:44 -0800

    sets up store, rootreducer, cat reducer

---
+++ src/reducers/catReducer.js
@@ -0,0 +1,23 @@
+
+
+const initialState = {
+  currentCat:{},
+  loading: false,
+  error: null
+}
+
+const catReducer = (state=initialState, action) =>{
+// if(action.type===FETCH_CAT_SUCCESS){
+//   return Object.assign({},state, {currentCat:action.cat, loading:false} )
+// }
+// else if(){
+
+// }
+// else if(){
+  
+// }
+
+return state;
+};
+
+export default catReducer;
