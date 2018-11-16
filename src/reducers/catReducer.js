bc5b1cce42801b33a07ffa55ca29a6f816aa2207
Janet Leon <leon.irenejanet@gmail.com>
2018-11-16 10:44:46 -0800

    adds cat reducer

--- src/reducers/catReducer.js
+++ src/reducers/catReducer.js
@@ -7,15 +7,15 @@ const initialState = {
 }
 
 const catReducer = (state=initialState, action) =>{
-// if(action.type===FETCH_CAT_SUCCESS){
-//   return Object.assign({},state, {currentCat:action.cat, loading:false} )
-// }
-// else if(){
-
-// }
-// else if(){
-  
-// }
+if(action.type===FETCH_CAT_SUCCESS){
+  return Object.assign({},state, {currentCat:action.cat, loading:false, error:null} )
+}
+else if(action.type===FETCH_CAT_REQUEST){
+  return Object.assign({},state, {loading:true, error:null} )
+}
+else if(action.type===FETCH_CAT_ERROR){
+  return Object.assign({},state, {loading:false, error:action.error} )
+}
 
 return state;
 };
